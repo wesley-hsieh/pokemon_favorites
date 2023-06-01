a293b010d7790624a8bd6056407ba92b4ae80ac7
Wesley Hsieh <53553252+wesley-hsieh@users.noreply.github.com>
2023-06-01 15:38:56 -0700

    Feat: bare bone template html files and app.py
    Setup template html files to prep for later as well as default homepage route to test with

---
+++ app.py
@@ -0,0 +1,29 @@
+import os
+
+from flask import Flask, render_template, request, flash, redirect, session, g
+from flask_debugtoolbar import DebugToolbarExtension
+from sqlalchemy.exc import IntegrityError
+
+
+CURR_USER_KEY = "curr_user"
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    os.environ.get('DATABASE_URL', 'postgresql:///pokemon-favorites'))
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = True
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+@app.route('/')
+def homepage():
+
+    users = User.query.all()
+
+    return render_template("index.html", users = users)
