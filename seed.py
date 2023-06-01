bcdd4920d2c3da0044e665b6f28bf7d5f9f939a8
Wesley Hsieh <53553252+wesley-hsieh@users.noreply.github.com>
2023-06-01 15:44:24 -0700

    Feat: create seed.py
    Create seed file to populate backend with data for testing.

---
+++ seed.py
@@ -0,0 +1,43 @@
+"""Seed database with sample data from CSV Files."""
+
+from app import db
+from models import User, Pokemon, Favorite
+
+import requests
+
+db.drop_all()
+db.create_all()
+
+red = User.register(
+    username="red",
+    pwd="password",
+    email="champion@email.com"
+)
+
+brock = User.register(
+    username="brock",
+    pwd="password",
+    email="rocksaregreat@email.com"
+)
+
+misty = User.register(
+    username="misty",
+    pwd="password",
+    email="waterisbetterdummy@email.com"
+)
+
+sabrina = User.register(
+    username="sabrina",
+    pwd="password",
+    email="ghostsaremyfriend@email.com"
+)
+
+brock_fav = Favorite(
+    user_id = 2,
+    pokemon_id = 1
+)
+
+db.session.add_all([red, brock, misty, sabrina, geodude, brock_fav])
+# db.session.add_all([pikachu, blastoise, venusaur, charizard, espeon, snorlax])
+
+db.session.commit()
