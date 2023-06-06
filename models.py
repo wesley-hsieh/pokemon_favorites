c62c871b4cc1fd4d4de79862a169f06c23ce718d
Wesley Hsieh <53553252+wesley-hsieh@users.noreply.github.com>
2023-06-06 14:56:18 -0700

    Feat: update models.py
    Added user_teams model: represent teams the user made themselves.
    Added Team model: represents the actual pokemon team using foreign relations to another model Team_pokemon.
    Added team_pokemon model: represents the  pokemon to be placed in the Team.
    Added Held_item, Move models which are fields to be put into team_pokemon.
    Added saved_teams model: represents relation between teams that the user saved/favorited to view later.

--- models.py
+++ models.py
@@ -16,6 +16,7 @@ class User(db.Model):
 
     favorites = db.relationship('Pokemon', secondary='favorites', backref='user')
     teams = db.relationship('Team', secondary='user_teams', backref="user")
+    saved_teams = db.relationship('Team', secondary='saved_teams', backref="user_saved")
 
     @classmethod
     def register(cls, username, pwd, email):
@@ -69,6 +70,94 @@ class Favorite(db.Model):
 
     user = db.relationship('User', backref="favorite")
     pokemon = db.relationship('Pokemon', backref="favorite")
+
+class User_teams(db.Model):
+    __tablename__ = "user_teams"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+
+    user = db.relationship('User', backref="user_teams")
+    team = db.relationship('Team', backref="user_teams")
+
+class Team(db.Model):
+    __tablename__ = "teams"
+
+    #Pokemon do not need to be unique (add docstring)
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    team_pokemon_1 = db.Column(db.Integer, db.ForeignKey('team_pokemon.id'))
+    team_pokemon_2 = db.Column(db.Integer, db.ForeignKey('team_pokemon.id'))
+    team_pokemon_3 = db.Column(db.Integer, db.ForeignKey('team_pokemon.id'))
+    team_pokemon_4 = db.Column(db.Integer, db.ForeignKey('team_pokemon.id'))
+    team_pokemon_5 = db.Column(db.Integer, db.ForeignKey('team_pokemon.id'))
+    team_pokemon_6 = db.Column(db.Integer, db.ForeignKey('team_pokemon.id'))
+
+    pokemon1 = db.relationship('Team_pokemon', foreign_keys=[team_pokemon_1])
+    pokemon2 = db.relationship('Team_pokemon', foreign_keys=[team_pokemon_2])
+    pokemon3 = db.relationship('Team_pokemon', foreign_keys=[team_pokemon_3])
+    pokemon4 = db.relationship('Team_pokemon', foreign_keys=[team_pokemon_4])
+    pokemon5 = db.relationship('Team_pokemon', foreign_keys=[team_pokemon_5])
+    pokemon6 = db.relationship('Team_pokemon', foreign_keys=[team_pokemon_6])
+
+
+class Team_pokemon(db.Model):
+    __tablename__ = "team_pokemon"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    pokemon_id = db.Column(db.Integer, db.ForeignKey('pokemon.id'))
+    move_1 = db.Column(db.Integer, db.ForeignKey('moves.id'))
+    move_2 = db.Column(db.Integer, db.ForeignKey('moves.id'))
+    move_3 = db.Column(db.Integer, db.ForeignKey('moves.id'))
+    move_4 = db.Column(db.Integer, db.ForeignKey('moves.id'))
+    ability = db.Column(db.Text)
+    ability_desc = db.Column(db.Text)
+    held_item = db.Column(db.Integer, db.ForeignKey("held_items.id"))
+
+    pokemon = db.relationship('Pokemon', backref="team_pokemon")
+    move1 = db.relationship('Move', foreign_keys=[move_1])
+    move2 = db.relationship('Move', foreign_keys=[move_2])
+    move3 = db.relationship('Move', foreign_keys=[move_3])
+    move4 = db.relationship('Move', foreign_keys=[move_4])
+
+    held_items = db.relationship('Held_item', backref="team_pokemon")
+
+class Held_item(db.Model):
+    __tablename__ = "held_items"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    desc = db.Column(db.Text, nullable=False)
+    sprites = db.Column(db.Text)
+
+class Move(db.Model):
+    __tablename__ = "moves"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.Text, nullable=False, unique=True)
+    power = db.Column(db.Integer)
+    damage_class = db.Column(db.Text)
+    accuracy = db.Column(db.Integer)
+    type = db.Column(db.Text)
+    desc = db.Column(db.Text)
+
+    def setDesc(self, desc):
+        self.desc = desc
+
+class Saved_teams(db.Model):
+    __tablename__ = "saved_teams"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
+
+# class Ability(db.Model):
+#     __tablename__ = "abilities"
+#
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     name = db.Column(db.Text, nullable=False)
+#     desc = db.Column(db.Text)
 
 def connect_db(app):
     """Connect to database."""
