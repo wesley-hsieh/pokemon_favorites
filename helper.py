e3bb914f32963f15ce5e1c468f3659bf023aa107
Wesley Hsieh <53553252+wesley-hsieh@users.noreply.github.com>
2023-06-06 15:00:52 -0700

    Refactor: queryPokemonByNameOrID
    Refactored queryPokemonByNameOrID in helper.py to encompass whether an integer or string is passed into function as both are accepted in the API.

--- helper.py
+++ helper.py
@@ -19,44 +19,64 @@ def createUser(name, pwd, email):
     db.session.commit()
 
 def queryPokemonByNameOrId(pokemon_name_id):
-    #grab pokemon data
-    request = requests.get(getPokemonURL+pokemon_name_id)
+    #TODO: check if pokemon in database
+    pokemonByName, pokemonByID = None, None
+    try:
+        pokemonByName = Pokemon.query.filter(Pokemon.name == pokemon_name_id).one_or_none()
+    except:
+        pokemonByID = Pokemon.query.filter(Pokemon.dex_number == pokemon_name_id).one_or_none()
+
+    #ternary operation to get which of the two queries evaluated to completion
+    query = pokemonByName if pokemonByName else pokemonByID
+
+    if query:
+        return query
+    else:
+        #grab pokemon data
+        request = requests.get(getPokemonURL+pokemon_name_id)
+        data = request.json()
+
+        #populate moves table with data(?)
+        for move in data["moves"]:
+            name = move["move"]["name"]
+
+            #query for move, making sure to replace the hyphen, if it does not exist, create move
+            query_count = Move.query.filter(Move.name.ilike(name.replace('-', ' '))).count()
+            if query_count == 0:
+                createMove(name)
+
+        #destructure the stats
+        health_data, attack_data, defence_data, special_atk_data, special_def_data, speed_data = data["stats"]
+
+        #destructure the types
+        type_1, *type_2 = data["types"]
+
+        #create list of just the actual desired value for simplicity
+        types = [type_1["type"]["name"]]
+
+        #conditional statement to test if type_2 even exists
+        if type_2:
+            types.append(type_2[0]["type"]["name"])
+
+        #create pokemon instance
+        queriedPokemon = Pokemon(
+            dex_number = data["id"],
+            name = data["name"],
+            sprites = data["sprites"]["front_default"],
+            shiny_sprites = data["sprites"]["front_shiny"],
+            health_stat = health_data["base_stat"],
+            attack_stat = attack_data["base_stat"],
+            defence_stat = defence_data["base_stat"],
+            special_atk_stat = special_atk_data["base_stat"],
+            special_def_stat = special_def_data["base_stat"],
+            speed_stat = speed_data["base_stat"]
+        )
+        #setTypes separately to take into cover both a singular type and multiple.
+        queriedPokemon.setTypes(types)
+
+        return queriedPokemon
+
     data = request.json()
 
-#     for key in data:
-#         print(key)
-    print(len(data["moves"]))
-
-    for move in data["moves"]:
-        print(move)
-
-    #destructure the stats
-    hp_data, atk_data, def_data, spatk_data, spdef_data, speed_data = data["stats"]
-
-    #destructure the types
-    type_1, *type_2 = data["types"]
-
-    #create list of just the actual desired value for simplicity
-    types = [type_1["type"]["name"]]
-
-    #conditional statement to test if type_2 even exists
-    if type_2:
-        types.append(type_2[0]["type"]["name"])
-
-    #create pokemon instance
-    queriedPokemon = Pokemon(
-        dex_number = data["id"],
-        name = data["name"],
-        sprites = data["sprites"]["front_default"],
-        shiny_sprites = data["sprites"]["front_shiny"],
-        hp_stat = hp_data["base_stat"],
-        atk_stat = atk_data["base_stat"],
-        def_stat = def_data["base_stat"],
-        spatk_stat = spatk_data["base_stat"],
-        spdef_stat = spdef_data["base_stat"],
-        speed_stat = speed_data["base_stat"]
     )
-    #setTypes separately to take into cover both a singular type and multiple.
-    queriedPokemon.setTypes(types)
 
-    return queriedPokemon
