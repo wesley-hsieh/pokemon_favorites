954b19ee9eb8aa543d95d9c50b2c8b74575cf1d2
Wesley Hsieh <whsieh1996@gmail.com>
2023-12-17 20:30:32 -0800

    added error troubleshooting

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 
 # print("os.environ", os.environ['DATABASE_URL'])"""  """
 uri = os.getenv("DATABASE_URL")
-if uri.startswith("postgres://"):
+if uri is not None and uri.startswith("postgres://"):
     uri = uri.replace("postgres://", "postgresql://", 1)
     app.config['SQLALCHEMY_DATABASE_URI'] = uri
 
